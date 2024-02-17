08f90d1b0de994760ca9a8dfa6b89b993806a9f8
Pinkee Maurya <akpipinkymaurya@gmail.com>
2024-02-17 19:40:41 +0530

    file is added .

---
+++ understandingstateupdatedvite/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+    console.log(count);
+  };
+
+  return (
+    <div>
+      <p>Button is clicked {count} times</p>
+      <button onClick={handleClick}>Click Me</button>
+    </div>
+  );
+}
+
+export default App;
+
